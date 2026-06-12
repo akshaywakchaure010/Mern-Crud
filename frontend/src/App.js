eab3d55a07af49fc5ca1b9474b317a490e9e1c48
unknown <wakchauream111@gmail.com>
2026-06-12 02:05:28 -0700

    updated app.js for api url

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function App() {
 
-const API = "https://mern-crud-golt.onrender.com/students";
+const API = "https://mern-crud-bvog.onrender.com/students";
 
   const [students, setStudents] = useState([]);
   const [name, setName] = useState("");
